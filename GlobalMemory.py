5db0594b1229c5073f0640bf104df63ea0420fae
Rafaelmor2000 <Rafaelmor2000@gmail.com>
2023-11-19 15:42:29 -0600

    Brought local and global memory up to standard

--- GlobalMemory.py
+++ GlobalMemory.py
@@ -1,5 +1,10 @@
 import sys
 
+INT = 10000
+FLOAT = 11000
+CHAR = 12000
+LIM = 13000
+
 
 class GlobalMemory:
     def __init__(self) -> None:
@@ -14,10 +19,10 @@ class GlobalMemory:
         varType = var.get("type")
 
         if varType == "int":
-            dir = 10000
+            dir = INT
             arrSize = int(var.get("arrSize"))
             dir += self.intCount + arrSize
-            if dir < 10000 or dir >= 11000:
+            if dir < INT or dir >= FLOAT:
                 print("no global memory for int variables available")
                 sys.exit()
             else:
@@ -31,10 +36,10 @@ class GlobalMemory:
                     self.intList.append(0)
 
         elif varType == "float":
-            dir = 11000
+            dir = FLOAT
             arrSize = int(var.get("arrSize"))
             dir += self.floatCount + arrSize
-            if dir < 11000 or dir >= 12000:
+            if dir < FLOAT or dir >= CHAR:
                 print("no global memory for float variables available")
                 sys.exit()
 
@@ -49,10 +54,10 @@ class GlobalMemory:
                     self.floatList.append(0.0)
 
         else:
-            dir = 12000
+            dir = CHAR
             arrSize = int(var.get("arrSize"))
             dir += self.charCount + arrSize
-            if dir < 12000 or dir >= 13000:
+            if dir < CHAR or dir >= LIM:
                 print("no global memory for char variables available")
                 sys.exit()
 
@@ -67,3 +72,31 @@ class GlobalMemory:
                     self.charList.append(" ")
 
         return dir
+
+    def getValue(self, dir):
+        if dir < INT or dir >= LIM:
+            print("Invalid direction for variable")
+            sys.exit()
+
+        elif dir < FLOAT:
+            return self.intList[dir - INT]
+
+        elif dir < CHAR:
+            return self.floatList[dir - FLOAT]
+
+        elif dir < LIM:
+            return self.charList[dir - CHAR]
+
+    def saveValue(self, dir, value):
+        if dir < INT or dir >= LIM:
+            print("Invalid direction for variable")
+            sys.exit()
+
+        elif dir < FLOAT:
+            self.intList[dir - INT] = value
+
+        elif dir < CHAR:
+            self.floatList[dir - FLOAT] = value
+
+        elif dir < LIM:
+            self.charList[dir - CHAR] = value
--- LocalMemory.py
+++ LocalMemory.py
@@ -1,5 +1,10 @@
 import sys
 
+INT = 20000
+FLOAT = 21000
+CHAR = 22000
+LIM = 23000
+
 
 class LocalMemory:
     def __init__(self) -> None:
@@ -14,10 +19,10 @@ class LocalMemory:
         varType = var.get("type")
 
         if varType == "int":
-            dir = 20000
+            dir = INT
             arrSize = int(var.get("arrSize"))
             dir += self.intCount + arrSize
-            if dir < 20000 or dir >= 21000:
+            if dir < INT or dir >= FLOAT:
                 print("no local memory for int variables available")
                 sys.exit()
             else:
@@ -28,11 +33,11 @@ class LocalMemory:
                     self.intCount += 1
 
         elif varType == "float":
-            dir = 21000
+            dir = FLOAT
             arrSize = int(var.get("arrSize"))
             dir += self.floatCount + arrSize
 
-            if dir < 21000 or dir >= 22000:
+            if dir < FLOAT or dir >= CHAR:
                 print("no local memory for float variables available")
                 sys.exit()
 
@@ -44,10 +49,10 @@ class LocalMemory:
                     self.floatCount += 1
 
         else:
-            dir = 22000
+            dir = CHAR
             arrSize = int(var.get("arrSize"))
             dir += self.charCount + arrSize
-            if dir < 22000 or dir >= 23000:
+            if dir < CHAR or dir >= LIM:
                 print("no local memory for char variables available")
                 sys.exit()
 
@@ -71,4 +76,63 @@ class LocalMemory:
         self.floatCount = 0
         self.charCount = 0
 
-        return reqMem
+        return
+
+    # Assign space and initialize local variables
+    def era(self, reqMem):
+        ints = reqMem.get("int")
+        self.intCount += ints
+        floats = reqMem.get("float")
+        self.floatCount += floats
+        chars = reqMem.get("char")
+        self.charCount += chars
+
+        # evaluate if there is enough space available, initialize and make space for function.
+        if self.intCount >= FLOAT:
+            print("no local memory for int variables available")
+            sys.exit()
+        else:
+            for i in range(ints):
+                self.intList.append(0)
+
+        if self.floatCount >= CHAR:
+            print("no local memory for float variables available")
+            sys.exit()
+        else:
+            for i in range(floats):
+                self.floatList.append(0.0)
+
+        if self.charCount >= LIM:
+            print("no local memory for char variables available")
+            sys.exit()
+        else:
+            for i in range(chars):
+                self.lCharList.append(" ")
+
+    def getValue(self, dir):
+        if dir < INT or dir >= LIM:
+            print("Invalid direction for variable")
+            sys.exit()
+
+        elif dir < FLOAT:
+            return self.intList[dir - INT]
+
+        elif dir < CHAR:
+            return self.floatList[dir - FLOAT]
+
+        elif dir < LIM:
+            return self.charList[dir - CHAR]
+
+    def saveValue(self, dir, value):
+        if dir < INT or dir >= LIM:
+            print("Invalid direction for variable")
+            sys.exit()
+
+        elif dir < FLOAT:
+            self.intList[dir - INT] = value
+
+        elif dir < CHAR:
+            self.floatList[dir - FLOAT] = value
+
+        elif dir < LIM:
+            self.charList[dir - CHAR] = value
